d6065d651fcf1e19a624ed4aa845fa869bc5f90e
Chris Margroff <sora3087@gmail.com>
2025-08-08 21:15:23 -0400

    update enemy mover to not be a parent of PathFollow node

--- components/enemies/EnemyMover.cs
+++ components/enemies/EnemyMover.cs
@@ -1,18 +1,22 @@
 using Godot;
 
-public partial class EnemyMover : PathFollow2D
+public partial class EnemyMover : Node2D
 {
+  [Export] Path2D Path;
   [Export] public float MoveSpeed = 30f;
+  private PathFollow2D _follower;
   private bool _done = false;
   public override void _EnterTree()
   {
-    Loop = false;
+    _follower = new PathFollow2D();
+    _follower.Loop = false;
+    AddChild(_follower);
+    MoveToPath(Path);
   }
 
   private void StartNextPath()
   {
-    var path = GetParent();
-    if (path is LinkedPath linkedPath && linkedPath.NextPath is not null)
+    if (Path is LinkedPath linkedPath && linkedPath.NextPath is not null)
     {
       MoveToPath(linkedPath.NextPath);
     }
@@ -21,19 +25,26 @@ public partial class EnemyMover : PathFollow2D
   public override void _Process(double delta)
   {
     if (_done) return;
-    Progress += (float)delta * MoveSpeed;
-    if (ProgressRatio >= 1f)
+    _follower.Progress += (float)delta * MoveSpeed;
+    if (_follower.ProgressRatio >= 1f)
     {
       _done = true;
       StartNextPath();
     }
   }
 
+  public override void _PhysicsProcess(double delta)
+  {
+    GlobalPosition = _follower.GlobalPosition;
+    GlobalRotation = _follower.GlobalRotation;
+  }
+
   public void MoveToPath(Path2D newPath)
   {
-    Reparent(newPath);
-    Progress = 0;
+    _follower.Reparent(newPath);
+    _follower.Progress = 0;
     _done = false;
+    Path = newPath;
   }
 
 }
